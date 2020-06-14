3ab4675eb162e22a1823a99aed5ac54728708dbf
Luis Caballero <Luis Caballero@192.168.1.8>
2020-06-14 18:57:44 -0300

    Subida de tp 04 veterinaria

---
+++ src/main/java/ar/edu/unju/edm/Edmtp4PvCaballeroLuisVeterinariaApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Edmtp4PvCaballeroLuisVeterinariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Edmtp4PvCaballeroLuisVeterinariaApplication.class, args);
+	}
+
+}
